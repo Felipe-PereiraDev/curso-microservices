e223ac2a6bb3fc0b0b708eff4e53cb91e8ce1d9b
Felipe-PereiraDev <felipe.pereirasilvaa@outlook.com>
2025-11-23 18:45:01 -0400

    feat(naming-server): adiciona configuração inicial do servidor Eureka

---
+++ naming-server/src/main/java/br/com/StartUp.java
@@ -0,0 +1,15 @@
+package br.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
